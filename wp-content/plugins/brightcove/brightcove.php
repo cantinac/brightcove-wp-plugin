349eb563d8cd9137861269fa45c5b765cab50e47
Nancy Decker <nancy@cantinaconsulting.com>
2012-03-15 11:29:19 -0400

    Installed wp and set up basic files for plugin

---
+++ wp-content/plugins/brightcove/brightcove.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Brightcove
+ * @version 0.1
+ */
+/*
+Plugin Name: Brightcove
+Plugin URI: 
+Description: Brightcove plugin
+Author: Nancy Decker
+Version: 0.1
+Author URI: 
+*/
+
+?>
